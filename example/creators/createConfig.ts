352c94d56f2f4ba25558afdffc3e4903421b64e6
Łukasz Pietrek <lukasz.pietrek@dazn.com>
2021-08-17 12:25:14 +0200

    Create configuration only once

--- example/creators/createConfig.ts
+++ example/creators/createConfig.ts
@@ -1,7 +1,8 @@
+import once from 'lodash/once';
 import {ConfigType} from '../types';
 
 export function createConfig(): () => ConfigType {
-    return () => {
+    return once(() => {
         const logLevel = process.env.IOC_LOG_LEVEL;
         if (logLevel && !['silent', 'verbose'].includes(logLevel)) {
             throw new Error(`Unrecognized logLevel "${logLevel}".`);
@@ -9,5 +10,5 @@ export function createConfig(): () => ConfigType {
         return <ConfigType>{
             logLevel: logLevel || 'verbose',
         };
-    };
+    });
 }
